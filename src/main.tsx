96e75dcc34062a7be306841830d39ffb2750da60
Rishabh Kumar <rajrishabhkr@gmail.com>
2025-07-15 18:52:23 +0530

    Initial commit5

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter } from 'react-router-dom' // ✅ import
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@/styles/index.scss'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/dashboard">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
