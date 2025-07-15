d5a406d5c5252b3ec33f1e4fd2c7f88928f44665
Rishabh Kumar <rajrishabhkr@gmail.com>
2025-07-15 18:56:45 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home';
 import Info from './pages/Info';
@@ -15,22 +15,20 @@ import Calculator from './pages/stocks/Calculator';
 const App = () => {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="info" element={<Info />} />
-            <Route path="students" element={<Students />} />
-            <Route path="teachers" element={<Teachers />} />
-            <Route path="attendance" element={<Attendance />} />
-            <Route path="tictactoe" element={<TicTacToe />} />
-            <Route path="3dcube" element={<Cube3D />} />
-            <Route path="ipolist" element={<IPOList />} />
-            <Route path="/ipolist/details/:id" element={<IPODetail />} />
-            <Route path="calculator" element={<Calculator />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="info" element={<Info />} />
+          <Route path="students" element={<Students />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="attendance" element={<Attendance />} />
+          <Route path="tictactoe" element={<TicTacToe />} />
+          <Route path="3dcube" element={<Cube3D />} />
+          <Route path="ipolist" element={<IPOList />} />
+          <Route path="ipolist/details/:id" element={<IPODetail />} />
+          <Route path="calculator" element={<Calculator />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
